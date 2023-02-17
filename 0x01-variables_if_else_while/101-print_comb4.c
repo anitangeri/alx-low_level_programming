a33a613d18beaae5ae2b53ff1f95860a9d7c1313
Anita Ngeri <anitangeri@gmail.com>
2023-02-16 23:02:45 -0800

    advanced tasks

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+	int i;
+	int j;
+
+	for (j = 0 ; j != 8 ; j++)
+	{
+		for (n = j + 1 ; n != 9 ; n++)
+		{
+			i = n + 1;
+			do {
+				putchar('0' + j);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (j != 7)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				i++;
+			} while (i != 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
